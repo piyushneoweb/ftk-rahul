47754be000cc61a08061d4b5bfecafb576b6bda0
Makinfosys <piyushneoweb@gmail.com>
2018-06-22 16:22:29 +0530

    theme designed by rahul

---
+++ theme_ftk_rm/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Theme FTK RM",
+    'version': '1.0',
+    'summary': 'Theme FTK RM',
+    'description': """
+Theme FKT RM.
+====================
+    """,
+    'author': "Rahul Makhare -rahul.makhare9@gmail.com",
+    'website': "http://www.odoo.com",
+    'category': 'Others',
+    'depends': ['website_sale'],
+    'data': [
+        'views/assets.xml',
+        'views/website_template.xml',
+    ],
+    'images': ['static/src/img/main_img.png'],
+    'license': 'OPL-1',
+}
